4be85f709bfb04a2db14272776d8632e41cdbdb7
rockingshan <shantanu.is.here@gmail.com>
2020-12-01 16:18:28 +0530

    Initial code

---
+++ active_analysis.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(here)
+library(dplyr)
+library(gapminder) 
+list_active = read.csv(here("", "4024002_LISTOFACTCUST.CSV"))
+## replace ' in column data, change to proper column names
+
+list_active$STB <- gsub("'","",list_active$STB)
+list_active$SC <- gsub("'","",list_active$SC)
+colnames(list_active)[10] <- "VC"
+colnames(list_active)[11] <- "STB"
+list_active <- list_active %>% mutate(VC.length = nchar(VC),  .after = 11) # get character length of vc
+list_active$VC.length <- gsub("8","GOSPELL",list_active$VC.length, fixed = TRUE)
+list_active$VC.length <- gsub("12","SAFEVIEW",list_active$VC.length, fixed = TRUE)
+list_active$VC.length <- gsub("16","ABV",list_active$VC.length, fixed = TRUE) #REPLACE LENGTHS TO CAS NAMES
+
+# list_export_cond = list_active %>% 
+#   select(ENTITY_CODE) %>% 
+#   distinct() 
+
+list_export_cond = list_active %>% 
+  select(CUSTOMER_NBR,ENTITY_CODE,ENTITY_NAME,MOBILE_PHONE) %>% 
+  distinct() #select only these column and then remove duplicates
+
+active_pivot = list_export_cond %>% group_by(PLAN_NAME) %>%
+  summarize(Plan_Count = n()) # make pivot table
+
+#head(list_active
+#wallet_filtered = filter(wallet, Plan.Details=="Alacarte Plan")
+#target = c("Star Jalsha @ 19", "Jalsha Movies @ 6")
+#wallet_filter_condition = filter(wallet_filtered, Service.Name %in% target)
+#export_condition = wallet_filter_condition %>% select(Customer.Nbr,Customer.Name,Unique.Id,Entity.Code,Entity.Name,Mobile,Service.Name)
+write.csv(list_export_cond, "LCOs.csv", row.names = TRUE)
