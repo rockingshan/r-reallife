628e39e3112987df2ff6563a58f5c70d496a7137
rockingshan <shantanu.is.here@gmail.com>
2020-12-02 13:18:35 +0530

    code cleanup and function create

--- Wallet_analysis.R
+++ Wallet_analysis.R
@@ -2,26 +2,69 @@ library(tidyverse)
 library(here)
 library(dplyr)
 library(readxl)
+library(stringr)
+
+#function definitions....
+
+area_wise_op <- function(wallet_in){
+  # get wallet input and filter on starting condition and export areawise
+  WALLET_AREA_BPC = wallet_in %>% filter(str_detect(Entity.Code, "MSW"))
+  write.csv(WALLET_AREA_BPC, "Berhampore.csv", row.names = FALSE)
+  WALLET_AREA_HLZ = wallet_in %>% filter(str_detect(Entity.Code, "HCS"))
+  write.csv(WALLET_AREA_HLZ, "Haldia.csv", row.names = FALSE)
+}
+
+plan_wise_op <- function(wallet_in){
+  #filter plan wise and alacarte and other and output
+  wallet_filtered_ala = filter(wallet_in, Plan.Details=="Alacarte Plan")
+  write.csv(wallet_filtered_ala, "Alacarte.csv", row.names = FALSE)
+  wallet_filtered_othr = filter(wallet_in, Plan.Details!="Alacarte Plan")
+  write.csv(wallet_filtered_othr, "PlanBouqets.csv", row.names = FALSE)
+}
+
+lco_pivot_table <- function(wallet_in){
+  ### Following blocks summarises code wise debit amount and export file --pivot table
+  lco_pivot = wallet_in %>% 
+    group_by(Entity.Code) %>%
+    summarize(Total_debit = sum(Amount.Debit))
+  write.csv(lco_pivot, "LCO WALLET SUMMARY.csv", row.names = FALSE)
+}
+
+lcowise_data_export <- function(wallet_in){
+  ## filter lco code and remove duplicate then convert to a list for loop
+  lco_list = wallet_in %>% select(Entity.Code) %>% distinct()
+  lco_list = lco_list[['Entity.Code']]
+  
+  ## run the loop according to the list and export csv for each LCO
+  for (lcocode in lco_list) {
+    wallet_filtered = filter(wallet_in, Entity.Code==lcocode)
+    write.csv(wallet_filtered, paste(lcocode,".csv", sep = ""), row.names = FALSE)
+  }
+}
+
+
+
 wallet = read.csv(here("data/4040924_WALLETSUMMLCONEW_NOV.CSV"))
 
-wallet_difference_prev = read.csv(here("data/LcoWiseWalletDebitDifference.CSV"))[,2:5]
+area_wise_op(wallet)
 
+plan_wise_op(wallet)
+
+lco_pivot_table(wallet)
+
+lcowise_data_export(wallet)
 
 
 # wallet_mod = read_excel(here("", "WALLET REPORT OCT2020.xlsx"))
 # names(wallet_mod) = make.names(names(wallet_mod))
 
 ### Following block filters and export files based on filter criteria
-wallet_filtered = filter(wallet, Plan.Details!="Alacarte Plan")
+
 # target = c("Star Jalsha @ 19", "Jalsha Movies @ 6")
 # wallet_filter_condition = filter(wallet_filtered, Service.Name %in% target)
 # export_condition = wallet_filter_condition %>% select(Customer.Nbr,Customer.Name,Unique.Id,Entity.Code,Entity.Name,Mobile,Service.Name)
-write.csv(wallet_filtered, "WithoutAlacarte.csv", row.names = FALSE)
 
-### Following blocks summarises code wise debit amount and export file --pivot table
-lco_pivot = wallet %>% 
-  group_by(Entity.Code) %>%
-  summarize(Total_debit = sum(Amount.Debit))
+
 
 # lco_pivot_modified = wallet_mod %>% 
 #   group_by(Entity.Code) %>%
@@ -29,19 +72,10 @@ lco_pivot = wallet %>%
 
 ### vlookup 
 #difference_debit = merge(lco_pivot_modified,lco_pivot) %>% mutate(difference = Total_debit.shantanu-Total_debit.mq)
-wallet_final_nov= merge(lco_pivot, wallet_difference_prev) %>% 
-  mutate(New.Wallet.total = Total_debit - difference) %>% 
-  select(Entity.Code,Total_debit,difference,New.Wallet.total)
+
+####following code is used if there are and adjustments need to made to the actual data
+# wallet_final_nov= merge(lco_pivot, wallet_difference_prev) %>% 
+#   mutate(New.Wallet.total = Total_debit - difference) %>% 
+#   select(Entity.Code,Total_debit,difference,New.Wallet.total)
 
 ### export
-write.csv(wallet_final_nov, "LCO WALLET SUMMARY NOV20.csv", row.names = FALSE)
-
-## filter lco code and remove duplicate then convert to a list for loop
-lco_list = wallet %>% select(Entity.Code) %>% distinct()
-lco_list = lco_list[['Entity.Code']]
-
-## run the loop according to the list and export csv for each LCO
-for (lcocode in lco_list) {
-  wallet_filtered = filter(wallet, Entity.Code==lcocode)
-  write.csv(wallet_filtered, paste(lcocode,".csv", sep = ""), row.names = FALSE)
-  }
