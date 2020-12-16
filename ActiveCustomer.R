3a683a7e295e7a37d8e6849330a8fb6e64853054
Shantanu <shantanu.is.here@gmail.com>
2020-12-16 14:13:04 +0530

    FILTER CONDITION

--- ActiveCustomer.R
+++ ActiveCustomer.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(here)
 library(dplyr)
 
-activ_cust = read.csv(here("data/4038273_StatewiseActiveCustomerReport.CSV"))
+activ_cust = read.csv(here("data/4108641_StatewiseActiveCustomerReport.CSV"))
 active_cust_for_lookup = activ_cust %>% select(Entity.Code,Entity.Name) %>% unique()
 
 active_pivot = activ_cust %>% 
@@ -10,5 +10,8 @@ active_pivot = activ_cust %>%
   summarize(Total_Active = sum(Active.Customer))
 active_pivot = merge(active_pivot, active_cust_for_lookup)
 active_pivot = active_pivot[, c(1,3,2)]
+#active_pivot_n = filter(active_pivot, grepl('MSW', Entity.Code,fixed = TRUE))
 
-write.csv(active_pivot, "ACTIVE_CUSTOMER_NOV20.csv", row.names = FALSE)
+
+write.csv(active_pivot, "LCOWISE_ACTIVE_CUST.csv", row.names = FALSE)
+#write.csv(active_pivot_n, "BPC_LCO_DATA.csv", row.names = FALSE)
