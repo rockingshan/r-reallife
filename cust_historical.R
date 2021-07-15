92c02156dee5ced6aa595a0217b90eb8121691db
rockingshan <shantanu.is.here@gmail.com>
2021-07-15 15:44:20 +0530

    proper

--- cust_historical.R
+++ cust_historical.R
@@ -3,11 +3,15 @@ library(tidyverse)
 library(here)
 library(dplyr)
 cas_code = read.csv("4451414_ServiceDetails.CSV")
-cust_historical <- read_csv("C:/Users/SHANTANU/Downloads/4446706_CUSTOMERS_NETWORK_14042020.CSV")
+cust_historical <- read_csv("5079444_CUSTOMERS_NETWORK.CSV")
 names(cust_historical)<-make.names(names(cust_historical),unique = TRUE)
+cust_hist_trim = select(cust_historical,Customer.Nbr,Set.Box.Number,Smartcard.Serialnumber,Name,Mobile) %>% unique()
+
+
+
 cust_historical <- cust_historical %>% mutate(VC.length = nchar(Smartcard.Serialnumber),  .after = 11)
 cust_historical$VC.length <- gsub("16","ABV",cust_historical$VC.length, fixed = TRUE)
 list_ac_ABV = filter(cust_historical, VC.length == "ABV") %>% select(Smartcard.Serialnumber,Package.Code)
 list_ac_ABV_codes = merge(list_ac_ABV,cas_code,all.x = TRUE) %>% na.omit(list_ac_ABV_codes, cols='Provision.Code')
 #VC_count = list_ac_ABV %>% select(Smartcard.Serialnumber) %>% unique()
-write.csv(list_ac_ABV_codes,"ABV_14042020.csv", row.names = F)
+write.csv(cust_hist_trim,"Customer_backdate.csv", row.names = F)
