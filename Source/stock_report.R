578c290920307b3ce9470bc225e1d7fa98e02255
rockingshan <shantanu.is.here@gmail.com>
2022-06-23 12:36:17 +0530

    Update stock_report.R

--- Source/stock_report.R
+++ Source/stock_report.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 inventory = read.csv(file.choose(new = F),colClasses = c(SERIAL_NUMBER="character")) ##inventory file
 customer_data = read.csv(file.choose(new = F)) ##customer master data
-lcoarea = read.csv(file.choose(new = F))
+#lcoarea = read.csv(file.choose(new = F))
 ####work on all area inventory file
 
 cardtype = c('Smart Card','ABV Smart Card','Sumavision SC','Gospell SC','Safeview SC')
@@ -19,5 +19,12 @@ inv_cust_data$Customer.Status <- gsub("A","Active",inv_cust_data$Customer.Status
 inv_cust_data$Customer.Status <- gsub("I","Inactive",inv_cust_data$Customer.Status)
 inv_cust_data$Customer.Status <- gsub("N","In LCO Store",inv_cust_data$Customer.Status)
 inv_cust_data$Customer.Status[is.na(inv_cust_data$Customer.Status)] <- 'In LCO Store'
-inv_cust_pivot = inv_cust_data %>% group_by(ENTITY_CODE,Customer.Status) %>% summarise(STB.Count = n())
-write.csv(inv_data_area,"Output/Inventory_customer.csv",row.names = F)
+inv_cust_pivot = inv_cust_data %>% group_by(ENTITY_CODE,Customer.Status) %>% summarise(STB.Count = n()) %>%
+  pivot_wider(names_from = Customer.Status,values_from = STB.Count)
+write.csv(inv_cust_pivot,"Output/inv_cust_pivot.csv",row.names = F)
+
+
+##find customers with one hardware
+
+inv_partial = inventory %>% group_by(CUSTOMER_NBR) %>% summarise(Hardware.Count = n())
+inv_partial = inv_partial %>% filter(Hardware.Count == 1)
