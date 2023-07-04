16dbf5ab0d94f1ff877641b3f481c08c25119c21
rockingshan <shantanu.is.here@gmail.com>
2023-07-04 15:34:20 +0530

    Update PMR_REPORT_MAKE_NEW.R

--- Source/Monthly/PMR_REPORT_MAKE_NEW.R
+++ Source/Monthly/PMR_REPORT_MAKE_NEW.R
@@ -24,23 +24,36 @@ Bronze_pivot = Bronze_merged %>% group_by(Channel) %>% summarize(Active_count = 
 Bronze_pivot = merge(Bronze_pivot,trai_names) %>% relocate(TRAI.name, .after = Channel)
 write.csv(Bronze_pivot,"Output/FTA_Channels.csv",row.names = F)
 
+
+
 #without bronze
 bq_try = list_bouquet_dated %>% group_by(Plan.Name,Bouquet) %>% select(Customer.Number,Bouquet,Plan.Name,) %>% unique() %>% summarize(Active_count = n()) %>% filter(!(Bouquet == "Bronze Basic"))
 Bouquet_merged = merge(bq_try,DF,by.x = "Bouquet", by.y = "Service.Name" ) %>% select(Plan.Name,Bouquet,Channel,Active_count)
 colnames(Bouquet_merged)[4] <- "Monthly.Subs.of.the.Channel"
 
-bc_bouquet_filtered = filter(Bouquet_merged, Plan.Name %in% plan_names$Plan.Name)
-bc_bouquet_filtered_bq = filter(bc_bouquet_filtered, Bouquet %in% bouquet_names$Bouquet)
-bc_bouquet_filtered_bq = add_column(bc_bouquet_filtered_bq, PackType ='DPO Pack with broadcaster bouquets',.after = 2)
 
-bc_bouquet_filtered_al = filter(bc_bouquet_filtered, !(Bouquet %in% bouquet_names$Bouquet))
+bc_bouquet_filtered = filter(bq_try, Plan.Name %in% plan_names$Plan.Name) %>% select(Plan.Name,Active_count)
+####calculate new pacakge
+plan_confg = read.csv(file.choose()) ##choose single pack config
+bc_dpo_spread= merge(bc_bouquet_filtered,plan_confg)
+bc_dpo_bouq = bc_dpo_spread %>% filter(X == "Bouquet")
+bc_dpo_bouq_merged = merge(bc_dpo_bouq,DF,by.x = "Bouquet", by.y = "Service.Name") %>% select(Plan.Name,Bouquet,Channel,Active_count)
+colnames(bc_dpo_bouq_merged)[4] <- "Monthly.Subs.of.the.Channel"
+
+#bc_bouquet_filtered_bq = filter(bc_dpo_bouq_merged, Bouquet %in% bouquet_names$Bouquet)
+bc_dpo_bouq_merged = add_column(bc_dpo_bouq_merged, PackType ='DPO Pack with broadcaster bouquets',.after = 2)
+
+bc_bouquet_filtered_al = bc_dpo_spread %>% filter(X == "Alacarte") %>% select(Plan.Name,Bouquet,Active_count)
 bc_bouquet_filtered_al = add_column(bc_bouquet_filtered_al, PackType ='DPO Pack with Alacarte',.after = 2)
+bc_bouquet_filtered_al = bc_bouquet_filtered_al %>% mutate(Channel = Bouquet,.after = 3)
+colnames(bc_bouquet_filtered_al)[2] <- "Channel"
+colnames(bc_bouquet_filtered_al)[4] <- "Monthly.Subs.of.the.Channel"
 
 bc_bouquet_filtered_noDPO = filter(Bouquet_merged, !(Plan.Name %in% plan_names$Plan.Name))
 bc_bouquet_filtered_noDPO = filter(bc_bouquet_filtered_noDPO, !(Plan.Name %in% c('DD Channels','Platinum Digital Postpaid')))
 bc_bouquet_filtered_noDPO = add_column(bc_bouquet_filtered_noDPO, PackType ='Broadcaster Bouquets',.after = 2)
 
-bC_bouqet_final = rbind(bc_bouquet_filtered_bq,bc_bouquet_filtered_al,bc_bouquet_filtered_noDPO)
+bC_bouqet_final = rbind(bc_dpo_bouq_merged,bc_bouquet_filtered_al,bc_bouquet_filtered_noDPO)
 bC_bouqet_final$Monthly.Subs.of.the.Channel = as.numeric(bC_bouqet_final$Monthly.Subs.of.the.Channel)
 bC_bouqet_final_pivot = bC_bouqet_final %>% group_by(Channel,PackType) %>% summarize(TotalMonthlySubs = sum(Monthly.Subs.of.the.Channel)) %>%
   pivot_wider(names_from = PackType,values_from = TotalMonthlySubs)
@@ -55,13 +68,32 @@ write.csv(bc_bq_final_pvt_trai, "Output/Broadcaster Bouquet report PMR.csv", row
 write.csv(bc_ala_pvt_trai, "Output/Broadcaster Alacarte report PMR.csv", row.names = F)
 
 ####For Quarterly report
-all_bouquet = filter(bq_try, Bouquet %in% bouquet_names$Bouquet)
-all_bouquet_pivot = all_bouquet %>% group_by(Bouquet) %>% summarize(Total = sum(Active_count))
+all_bouquet = filter(bq_try, !(Plan.Name %in% plan_names$Plan.Name))
+all_bouquet_pivot = all_bouquet %>% group_by(Plan.Name) %>% summarize(Total = sum(Active_count))
 write.csv(all_bouquet_pivot,"Output/Bouqet_count.csv")
 
-all_dpopack = list_bouquet_dated %>% select(Customer.Number,Bouquet,Plan.Name) %>% unique() %>% filter(Bouquet=="Bronze Basic") %>%
-  group_by(Plan.Name,Bouquet) %>% summarize(Active_count = n()) %>% 
-  filter(Plan.Name %in% plan_names$Plan.Name)  %>% select(Plan.Name,Active_count) %>% unique() %>%
+all_dpopack = filter(bq_try, (Plan.Name %in% plan_names$Plan.Name)) %>%
   group_by(Plan.Name) %>% summarize(Total = sum(Active_count))
 
 write.csv(all_dpopack,"Output/DPO_count.csv")
+
+###for mcbs old company
+##############
+custom_num = list_bouquet_dated %>% select(Customer.Number) %>% unique()
+set.seed(123)
+total_rows <- nrow(custom_num)
+random_indices <- sample(total_rows, 1000)
+subset_dataset <- custom_num[random_indices, ]
+subset_dataframe <- data.frame(subset_dataset)
+colnames(subset_dataframe)[1] <- "Customer.Number"
+bq_try_mcbs = merge(list_bouquet_dated,subset_dataframe)
+bq_try_mcbs = bq_try_mcbs %>% group_by(Plan.Name,Bouquet) %>% select(Customer.Number,Bouquet,Plan.Name,) %>% unique() %>% summarize(Active_count = n()) %>% filter(!(Bouquet == "Bronze Basic"))
+
+all_bouquet = filter(bq_try_mcbs, !(Plan.Name %in% plan_names$Plan.Name))
+all_bouquet_pivot = all_bouquet %>% group_by(Plan.Name) %>% summarize(Total = sum(Active_count))
+write.csv(all_bouquet_pivot,"Output/mcbs_Bouqet_count.csv")
+
+all_dpopack = filter(bq_try_mcbs, (Plan.Name %in% plan_names$Plan.Name)) %>%
+  group_by(Plan.Name) %>% summarize(Total = sum(Active_count))
+
+write.csv(all_dpopack,"Output/mcbs_DPO_count.csv")
